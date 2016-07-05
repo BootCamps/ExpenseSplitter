8bd5bdc8f495a1d2f85eccb200365eaa3bae5daf
unknown <gdevakum@WGC1002PTBL72.chennai.ford.com>
2016-07-05 17:01:49 +0530

    login controller spec changes

--- ExpenseSplitterWeb/WebContent/application/js/login/loginController.js
+++ ExpenseSplitterWeb/WebContent/application/js/login/loginController.js
@@ -1,5 +1,9 @@
-function login(){
-	this.user = function(username, password){
+function login(loginService){
+	this.validUser = function(username, password){
 		
+		if(loginService.validateUser(username, password)){
+			return "Success";
+		}
+		return "Username/Password is invalid";
 	}
 }
